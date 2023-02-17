47ce92010346c6a771ef7853a4b11aaf1c3debb8
MacBook Air <macbookair@air-de-macbook.home>
2023-02-17 14:10:16 +0100

    v2

--- main.py
+++ main.py
@@ -1,37 +1,3 @@
-
-# import random
-#
-# prix = random.randint(1,10)
-#
-#
-# rep = input("Devinez le juste prix ! Le prix est un nombre compris entre 1 et 10 inclus.")
-# while intrep != prix :
-#     intrep = int(rep)
-#     tent = 1
-#
-#     if intrep == prix :
-#         print("Félicitations, vous avez trouvé le juste prix", prix, "en", tent, "essais, votre score est !")
-#         print("Partie terminée")
-#
-#     while rep != prix :
-#
-#         if intrep > 10:
-#             print("Veuillez saisir un chiffre compris entre 1 et 10 inclus")
-#             tent = tent + 1
-#             break
-#
-#         if intrep < prix:
-#             print("Le juste prix est plus haut")
-#             tent = tent + 1
-#             break
-#         if intrep > prix:
-#             print("Le juste prix est plus bas")
-#             tent = tent + 1
-#             break
-
-#print(prix)
-#print(rep)
-
 import random
 
 prix = random.randint(1, 10)
